76dd275b6b9e655c2edd0d0287cae074da245b2a
Taehyun Kim <kimtaehyun912@gmail.com>
2024-11-13 18:32:13 +0900

    24.11.13
    BackGround, Player 에셋 적용,
    Player 왼쪽으로 움직일 때 x값 반전 적용

--- Assets/Script/PlayerMove/PlayerAMove.cs
+++ Assets/Script/PlayerMove/PlayerAMove.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerAMove : MonoBehaviour
 {
-    public float maxSpeed; // 최대 속도
+    public float curSpeed; // 최대 속도
     private Rigidbody2D rigid;
     public GameObject ground; //Ground 오브젝트
     private float minX, maxX; // 이동 가능한 x축 범위
+    private SpriteRenderer rendererA; // 이미지 좌우반전
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        rendererA = GetComponent<SpriteRenderer>();
 
         if (ground != null)
         {   // Ground 오브젝트의 중심위치,크기 가져옴
@@ -33,14 +35,16 @@ public class PlayerAMove : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) // A 키가 눌렸다면
         {
             h = -1; // 왼쪽으로 이동
+            rendererA.flipX = true; // 이미지 반전
         }
         else if (Input.GetKey(KeyCode.D)) // D 키가 눌렸다면
         {
             h = 1; // 오른쪽으로 이동
+            rendererA.flipX = false; // 이미지 그대로
         }
 
         // 속도 설정 (미끄러짐 방지)
-        rigid.velocity = new Vector2(h * maxSpeed, rigid.velocity.y);
+        rigid.velocity = new Vector2(h * curSpeed, rigid.velocity.y);
         
         //Ground 범위 내로 이동 제한
         Vector3 clampedPosition = transform.position;
