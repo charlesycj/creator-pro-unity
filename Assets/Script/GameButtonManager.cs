88ecb21963fe469b749d47d01a7662c4443df84e
KimTaehyun7 <kimtaehyun912@gmail.com>
2024-12-26 19:23:32 +0900

    feat : resume버튼 추가
    게임 일시정지 시 pause버튼이 resume버튼으로 바뀌게 개발

--- Assets/Script/GameButtonManager.cs
+++ Assets/Script/GameButtonManager.cs
@@ -7,6 +7,9 @@ public class GameButtonManager : MonoBehaviour
 {
     private bool isPaused = false; // 게임이 일시정지 상태인지 여부를 저장
 
+    public GameObject pauseButton;  // Pause 버튼
+    public GameObject resumeButton; // Resume 버튼
+
     // EXIT 버튼 
     public void ExitGame()
     {
@@ -22,12 +25,20 @@ public class GameButtonManager : MonoBehaviour
             // 게임 재개
             Time.timeScale = 1f; // 게임 속도를 정상으로 설정
             isPaused = false;
+
+            // 버튼 상태 변경
+            resumeButton.SetActive(false); // Resume 버튼 비활성화
+            pauseButton.SetActive(true);  // Pause 버튼 활성화
         }
         else
         {
             // 게임 일시정지
             Time.timeScale = 0f; // 게임 속도를 0으로 설정
             isPaused = true;
+
+            // 버튼 상태 변경
+            resumeButton.SetActive(true); // Resume 버튼 활성화
+            pauseButton.SetActive(false); // Pause 버튼 비활성화
         }
     }
 
