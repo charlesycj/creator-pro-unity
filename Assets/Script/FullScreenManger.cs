fcef39e86ea484b7aa26260528704309738ab2d5
KimTaehyun7 <kimtaehyun912@gmail.com>
2025-01-02 18:22:57 +0900

    fix : 해상도 고정 코드 수정
    FHD해상도 고정 코드 수정

--- Assets/Script/FullScreenManger.cs
+++ Assets/Script/FullScreenManger.cs
@@ -1,26 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class FullScreenManager : MonoBehaviour
+public class Resolution : MonoBehaviour
 {
-    void Start()
+    public int setWidth = 1920; // 사용자 설정 너비
+    public int setHeight = 1080; // 사용자 설정 높이
+
+    private void Start()
     {
-
-        // 해상도를 1920x1080으로 고정
-        int targetWidth = 1920;
-        int targetHeight = 1080;
-
-        // 해상도와 풀스크린 모드 설정
-        Screen.SetResolution(targetWidth, targetHeight, true);
-        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
-
-        // 위의 코드나 아래 코드중 하나만 활성화해주세요
-
-        
-       /*// 현재 화면 해상도 가져오기
-        Resolution currentResolution = Screen.currentResolution;
-
-        // 해상도와 풀스크린 모드 설정
-        Screen.SetResolution(currentResolution.width, currentResolution.height, true);
-        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;*/
+        SetResolution(); // 초기에 게임 해상도 고정
     }
+
+    /* 해상도 설정하는 함수 */
+    public void SetResolution()
+    {
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
+
+
 }
